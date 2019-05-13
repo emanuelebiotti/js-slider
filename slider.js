657ae3f2c6877db32e28bb3e763bf0722f305e18
emanuele <emanuele.biotti@gmail.com>
2019-05-13 10:36:23 +0200

    aggiunta scorrimento a sinistra

--- slider.js
+++ slider.js
@@ -18,15 +18,20 @@ $('.next').click(function(){
 });
 
 $('.prev').click(function(){
+  //prendo l'immagine corrente
   var current_img = $('.visible');
-  current_img.removeClass('visible');
+  //al click rimuovo la classe visible a tutte le img
+  $('.slider img').removeClass('visible');
+  //definisco qual è l'immagine successiva
   var prev_img = current_img.prev('img');
 
+//se un'immagine successiva esiste:
   if(prev_img.length != 0) {
-    prev_img.addClass('.visible');
+    // aggiungo la classe visible all'img successiva
+    prev_img.addClass('visible');
+    //se invece non c'è alcuna immagine successiva
   } else {
+    //riparto dalla prima img e la setto come visibile
     $('.slider img').last().addClass('visible');
   }
 });
-
-//ok per avanzamento a destra ma non ancora a sinistra
